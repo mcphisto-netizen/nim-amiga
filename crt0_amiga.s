3a663503298c009f9f8685376e71b3caa2c50bca
mcphisto-netizen <mcphisto@gmail.com>
2026-03-18 18:42:01 -0300

    Update crt0_amiga.s

--- crt0_amiga.s
+++ crt0_amiga.s
@@ -1,5 +1,6 @@
-    ; Minimal crt0 for AmigaOS 1.3 (m68k, no clib2)
-    ; Initializes SysBase and DOSBase, then calls main()
+    ; Minimal crt0 for AmigaOS 1.3
+    ; Only initializes SysBase and DOSBase
+    ; Graphics/Intuition opened on-demand by modules
 
     SECTION code,code
 
@@ -9,28 +10,21 @@
     XREF _DOSBase
 
 _start:
-    ; SysBase is at address 4 (AmigaOS convention)
     move.l 4.w,a6
     move.l a6,_SysBase
 
-    ; Open dos.library (version 0 = any)
     lea dosname(pc),a1
     moveq #0,d0
-    jsr -552(a6)          ; OpenLibrary
+    jsr -552(a6)
     move.l d0,_DOSBase
-    beq.s .no_dos         ; Handle failure
 
-    ; Call main()
     jsr _main
 
-    ; Close dos.library
-.no_dos
     move.l _DOSBase,a1
-    beq.s .exit
+    beq.s .no_dos
     move.l _SysBase,a6
-    jsr -414(a6)          ; CloseLibrary
-
-.exit
+    jsr -414(a6)
+.no_dos
     moveq #0,d0
     rts
 
