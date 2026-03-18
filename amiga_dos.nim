575255ea81cb44ea02683e10a7fde2492078f7cf
mcphisto-netizen <mcphisto@gmail.com>
2026-03-18 18:16:10 -0300

    Update amiga_dos.nim

--- amiga_dos.nim
+++ amiga_dos.nim
@@ -1,6 +1,6 @@
 # Wrappers Nim -> dos.library (IO nativo)
 {.deadCodeElim: on.}
-{.inline: on.}
+{.push inline.}
 
 type
   BPTR* = uint32
@@ -12,8 +12,15 @@ proc read*(fh: BPTR; buf: pointer; len: LONG): LONG {.importc: "nim_read".}
 proc write*(fh: BPTR; buf: pointer; len: LONG): LONG {.importc: "nim_write".}
 proc close*(fh: BPTR): LONG {.importc: "nim_close".}
 proc output*(): BPTR {.importc: "nim_output".}
+proc lock*(name: STRPTR; access: LONG): BPTR {.importc: "nim_lock".}
+proc unlock*(lock: BPTR) {.importc: "nim_unlock".}
+proc examine*(lock: BPTR; fib: pointer): LONG {.importc: "nim_examine".}
+proc nextDosEntry*(lock: BPTR; fib: pointer; mode: LONG): LONG
+  {.importc: "nim_nextdosentry".}
 
 const
   MODE_OLDFILE* = 1005'i32
   MODE_NEWFILE* = 1006'i32
   MODE_READWRITE* = 1004'i32
+
+{.pop.}
